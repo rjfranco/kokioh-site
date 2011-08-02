c7ce5c946874841becdb231512cca75c245aa4fa
Ramiro Franco <rjfranco@gmail.com>
2011-08-01 17:44:00 -0700

    Committing initial file repository, prior to any changes.

---
+++ README.markdown
@@ -0,0 +1,4 @@
+#Kokioh Website
+## Rework of original code to better suit the logic needed to for the application
+
+A website framework to display the portfolio of Angela Vawser.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kokioh::Application.config.secret_token = 'b9c6da76ebc41b2d5e5ae1f800fbbea7beb7d4f274f8209d3800fb5d180905450338ad11be879adab02bcad1e06b88070596b7f18bf50cd8d93345b13ca774be'
